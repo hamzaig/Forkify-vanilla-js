99d8397af0642270044c27e12aa40b64cc006204
hamzaig <hamzaig@yahoo.com>
2021-09-26 22:24:43 +0500

    Loading a Recipe from API

--- src/js/controller.js
+++ src/js/controller.js
@@ -12,4 +12,30 @@ const timeout = function (s) {
 
 ///////////////////////////////////////
 
+const showRecipe = async () => {
+  try {
+    const response = await fetch("https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886");
+    const data = await response.json();
 
+    if (!response.ok) throw new Error(`${data.message} ${response.status}`)
+
+    let recipe = data.data.recipe;
+    console.log(recipe);
+    recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    }
+    console.log(recipe);
+    // console.log(response, data);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+showRecipe();
